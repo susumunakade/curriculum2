2cae5c87730f0456cbd19827e165302efd290819
susumunakade <ssm23@giga.ocn.ne.jp>
2023-02-05 20:20:37 +0900

    3章　練習問題2
    確認よろしくお願いします

--- 3-3/index.php
+++ 3-3/index.php
@@ -10,14 +10,16 @@
     <form action="result.php" method="post">
         <p>名前：<input type="text" name="my_name" /></p>
         <p>番号（1～6入力):<input type="text" name="number" /><p>
-            <!-- <select name="number">
-                <?php for($i=1;$i<=6;$i++){ ?>
+        <p><input type="submit" value="送信" /></p>
+    </form>
+</body>
+</html>
+
+
+<!-- <select name="number">
+<?php for($i=1;$i<=6;$i++){ ?>
                     <option value="<?php echo $i; ?>">
                         <?php echo $i; ?>  
                     </option>
                 <?php } ?>
             </select></p> -->
-        <p><input type="submit" value="送信" /></p>
-    </form>
-</body>
-</html>
