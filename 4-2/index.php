3846886be3d88f1d2230af191c23a80c0c20ec99
susumunakade <ssm23@giga.ocn.ne.jp>
2023-03-07 19:45:45 +0900

    4章チェックテスト
    課題提出します。

--- 4-2/index.php
+++ 4-2/index.php
@@ -5,26 +5,6 @@
     $users = $pdo->getUserData();
     $posts = $pdo->getPostData();
 
-
-    
-    // $sql = "SELECT * FROM users";
-    // // // 関数db_connect()からPDOを取得する
-    // $pdo = db_connect();
-    // try {
-        // $stmt = $pdo->prepare($sql);
-        // $stmt->execute();
-    
-    // $sql_stmt =$pdo-> query($sql);
-    // //     // ループ文を使用して、1行ずつ読み込んで$rowに代入する
-    // //     // 読み込むものがなくなったらループ終了
-        // while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-            // echo $row['id'] . '、' . $row['first_name'] . '、' . $row['last_name'];
-            // echo '<br />';
-        // }
-    // } catch (PDOException $e) {
-        // echo 'Error: ' . $e->getMessage();
-        // die();
-    // }
 ?>
 
 <!doctype html>
@@ -68,7 +48,7 @@
                 <p class="comment_column"> <?php echo '本文'; ?></p>
                 <p class="created_column"> <?php echo '投稿日'; ?></p>
             </div>
-            <?php try {  /* SQL処理を実行してDB postデータを出力*/ 
+            <?php try {  /* SQL処理を実行してDB postsデータを出力*/ 
         
                 // ループ文を使用して、1行ずつ読み込んで$rowに代入する
                 // 読み込むものがなくなったらループ終了
