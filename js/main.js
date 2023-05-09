e9d02c70022cf8df746b8493b948e016556adc63
susumunakade <ssm23@giga.ocn.ne.jp>
2023-05-09 19:12:02 +0900

    ルーレット

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-// let tm = 0;
 var roulette;
+var rnd;
+var confirm = new Array(17);
+var i = 1;
 
-function roulette_start(){
+
+function roulette_start() {
     roulette = setInterval(function() {
-        // 1〜99の範囲でランダムな数値を作成
-        var rnd = Math.floor( Math.random() * 16 ) + 1;
+        // ランダムな数値を作成
+        rnd = Math.floor( Math.random() * 16 ) + 1;
         // ルーレット
         change = document.getElementById('num_'+rnd);
         change.style.backgroundColor = 'red';
@@ -13,77 +16,36 @@ function roulette_start(){
             change.style.backgroundColor = '';
         }
 
-        setTimeout(fn,500);      
-
+        setTimeout(fn,500);       
                 
     } ,1000);   
-    
-    console.log(rnd);
-}
+
+
+};
 
 
 function roulette_stop() {
     if(roulette) {
         clearInterval(roulette);
+        // console.log(rnd);  
+    }
+
+    if (confirm[i]==null){
+        confirm[i] = document.getElementById('num_'+rnd);
+        console.log(confirm[i]);
+        confirm[i].style.backgroundColor = 'red';
+        i++;
+        console.log(i);
     }
 }
 
-
-
-// function roulette_start(){
-    
-//     for(let i = 1; i <= 50;i++){ 
-    
-        
-//         var rnd = Math.floor(Math.random() * 16)+1;
-//         let change = document.getElementById('num_' + rnd);
-
-//         var fn = function(){
-
-//         change.style.backgroundColor = 'red';
-        
-//         }
-    
-
-//         setTimeout(fn,tm);
-//         tm += 500;
-
-
-// //break
-
-
-//         var fn = function(){
-//         change.style.backgroundColor = '';
-//         }
-        
-//         // console.log(change);
-//         setTimeout(fn,tm);
-//         tm += 500;
-
-        
-                
-
-//         // function rulet_stop(){
-//         //     // console.log(fin);
-//         //     let stop_no = document.getElementById('num_' + rnd)
-//         //     stop_no.style.backgroundColor = 'red';
-//         // }
-         
-//     }
-
-//     console.log('num_' + rnd);
-//     // let stop_num = rnd;
-//     return rnd;
-      
-    
-// }
-// var confirm_no = roulette_start()
-// // var confirm_fin = document.getElementById('num_'+ confirm_no);
-// // confirm_no.style.backgroundColor = 'red'; 
-// console.log(confirm_no);
-
-
-
-
+function reset_num() {
+    for( i = 1; i <= confirm.length; i++){
+        confirm[i] = document.getElementById('num_' + i);
+        console.log(confirm[i]);
+        confirm[i].style.backgroundColor = '';
+        confirm[i] = '';
+    }
+}
 
 
